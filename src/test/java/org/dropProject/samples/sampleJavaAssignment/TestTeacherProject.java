f3c3cf4f18decadba6b7ae35d740eb8b6a277b89
Pedro Alves <pedro.alves@ulusofona.pt>
2022-12-14 11:35:41 +0000

    set timeout for all tests

--- src/test/java/org/dropProject/samples/sampleJavaAssignment/TestTeacherProject.java
+++ src/test/java/org/dropProject/samples/sampleJavaAssignment/TestTeacherProject.java
@@ -10,25 +10,25 @@ import static org.junit.Assert.assertEquals;
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class TestTeacherProject {
 
-    @Test
+    @Test(timeout = 500)
     public void test_001_FindMax() {
         assertEquals("trying to find the maximum in [1,2,7,4]", 7, Main.findMax(new int[] { 1, 2, 7, 4 }));
         assertEquals("trying to find the maximum in [1,2,4]", 4, Main.findMax(new int[] { 1, 2, 4 }));
     }
 
-    @Test
+    @Test(timeout = 500)
     public void test_002_FindMaxAllNegative() {
         assertEquals("trying to find the maximum in [-7,-5,-3,-1]", -1, Main.findMax(new int[]{-7, -5, -3, -1}));
         assertEquals("trying to find the maximum in [-7,-5,-3,-99]", -3, Main.findMax(new int[]{-7, -5, -3, -99}));
     }
 
-    @Test
+    @Test(timeout = 500)
     public void test_003_FindMaxNegativeAndPositive() {
         assertEquals("trying to find the maximum in [-7,-5,-3,-1]", 3, Main.findMax(new int[]{-7, -5, 3, -1}));
         assertEquals("trying to find the maximum in [-7,5,-3,-99]", 5, Main.findMax(new int[]{-7, 5, -3, -99}));
     }
 
-    @Test(expected = IllegalArgumentException.class)
+    @Test(expected = IllegalArgumentException.class, timeout = 500)
     public void test_004_FindMaxWithNull() {
         Main.findMax(null);
     }
