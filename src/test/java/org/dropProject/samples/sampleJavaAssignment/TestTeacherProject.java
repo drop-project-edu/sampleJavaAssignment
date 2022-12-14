a49b31aa27650b7eb09b722d818973e411b2d711
Pedro Alves <pedro.alves@ulusofona.pt>
2022-12-14 11:34:14 +0000

    enforce order of tests

--- src/test/java/org/dropProject/samples/sampleJavaAssignment/TestTeacherProject.java
+++ src/test/java/org/dropProject/samples/sampleJavaAssignment/TestTeacherProject.java
@@ -1,33 +1,36 @@
 package org.dropProject.samples.sampleJavaAssignment;
 
+import org.junit.FixMethodOrder;
 import org.junit.Test;
+import org.junit.runners.MethodSorters;
 
 import static org.junit.Assert.assertEquals;
 
 // in Drop Project, all test classes must begin with "Test"
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class TestTeacherProject {
 
     @Test
-    public void testFindMax() {
+    public void test_001_FindMax() {
         assertEquals("trying to find the maximum in [1,2,7,4]", 7, Main.findMax(new int[] { 1, 2, 7, 4 }));
         assertEquals("trying to find the maximum in [1,2,4]", 4, Main.findMax(new int[] { 1, 2, 4 }));
     }
 
-    @Test(expected = IllegalArgumentException.class)
-    public void testFindMaxWithNull() {
-        Main.findMax(null);
-    }
-
     @Test
-    public void testFindMaxAllNegative() {
+    public void test_002_FindMaxAllNegative() {
         assertEquals("trying to find the maximum in [-7,-5,-3,-1]", -1, Main.findMax(new int[]{-7, -5, -3, -1}));
         assertEquals("trying to find the maximum in [-7,-5,-3,-99]", -3, Main.findMax(new int[]{-7, -5, -3, -99}));
     }
 
     @Test
-    public void testFindMaxNegativeAndPositive() {
+    public void test_003_FindMaxNegativeAndPositive() {
         assertEquals("trying to find the maximum in [-7,-5,-3,-1]", 3, Main.findMax(new int[]{-7, -5, 3, -1}));
         assertEquals("trying to find the maximum in [-7,5,-3,-99]", 5, Main.findMax(new int[]{-7, 5, -3, -99}));
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void test_004_FindMaxWithNull() {
+        Main.findMax(null);
     }
 
 }
